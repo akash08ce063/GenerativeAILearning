5dfcd75275b8d1d764e622d8fdf2add602e2abd5
Akash Kanubhai Patel <akaspatel@microsoft.com>
2024-11-16 12:15:56 -0800

    postgres store index

---
+++ sample_codes/pgvector_store_index.py
@@ -0,0 +1,82 @@
+import os
+from langchain_core.documents import Document
+from langchain_postgres import PGVector
+from langchain_postgres.vectorstores import PGVector
+from huggingface_hub import login
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
+
+os.environ["HF_TOKEN"] = "XXX"
+os.environ['HUGGINGFACEHUB_API_TOKEN'] = "XXX"
+login(os.environ["HF_TOKEN"])
+
+connection = "postgresql+psycopg://langchain:langchain@localhost:6024/langchain"
+collection_name = "my_vectors"
+
+model_name = "BAAI/bge-small-en"
+model_kwargs = {"device": "cpu"}
+encode_kwargs = {"normalize_embeddings": True}
+hf = HuggingFaceBgeEmbeddings(
+    model_name = model_name,
+    model_kwargs = model_kwargs,
+    encode_kwargs = encode_kwargs
+)
+
+vector_store = PGVector(
+    embeddings=hf,
+    collection_name=collection_name,
+    connection=connection,
+    use_jsonb=True
+)
+
+docs = [
+    Document(
+        page_content="there are cats in the pond",
+        metadata={"id": 1, "location": "pond", "topic": "animals"},
+    ),
+    Document(
+        page_content="ducks are also found in the pond",
+        metadata={"id": 2, "location": "pond", "topic": "animals"},
+    ),
+    Document(
+        page_content="fresh apples are available at the market",
+        metadata={"id": 3, "location": "market", "topic": "food"},
+    ),
+    Document(
+        page_content="the market also sells fresh oranges",
+        metadata={"id": 4, "location": "market", "topic": "food"},
+    ),
+    Document(
+        page_content="the new art exhibit is fascinating",
+        metadata={"id": 5, "location": "museum", "topic": "art"},
+    ),
+    Document(
+        page_content="a sculpture exhibit is also at the museum",
+        metadata={"id": 6, "location": "museum", "topic": "art"},
+    ),
+    Document(
+        page_content="a new coffee shop opened on Main Street",
+        metadata={"id": 7, "location": "Main Street", "topic": "food"},
+    ),
+    Document(
+        page_content="the book club meets at the library",
+        metadata={"id": 8, "location": "library", "topic": "reading"},
+    ),
+    Document(
+        page_content="the library hosts a weekly story time for kids",
+        metadata={"id": 9, "location": "library", "topic": "reading"},
+    ),
+    Document(
+        page_content="a cooking class for beginners is offered at the community center",
+        metadata={"id": 10, "location": "community center", "topic": "classes"},
+    ),
+]
+
+vector_store.add_documents(docs, ids=[doc.metadata["id"] for doc in docs])
+
+results = vector_store.similarity_search(
+    "kitty", k=10, filter={"id": {"$in": [1, 5, 2, 9]}}
+)
+for doc in results:
+    print(f"* {doc.page_content} [{doc.metadata}]")
+
+print("vectors added in pg-store")
