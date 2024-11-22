96ea0acb529707442306ddae9ef35102739e0f66
Akash Kanubhai Patel <akaspatel@microsoft.com>
2024-11-21 21:50:59 -0800

    adding changes to repo

---
+++ sample_codes/Training/text_to_sql_few_shot.py
@@ -0,0 +1,65 @@
+from langchain_core.prompts import FewShotPromptTemplate, PromptTemplate
+from langchain_ollama import ChatOllama
+
+examples = [
+    {"input": "List all artists.", "query": "SELECT * FROM Artist;"},
+    {
+        "input": "Find all albums for the artist 'AC/DC'.",
+        "query": "SELECT * FROM Album WHERE ArtistId = (SELECT ArtistId FROM Artist WHERE Name = 'AC/DC');",
+    },
+    {
+        "input": "List all tracks in the 'Rock' genre.",
+        "query": "SELECT * FROM Track WHERE GenreId = (SELECT GenreId FROM Genre WHERE Name = 'Rock');",
+    },
+    {
+        "input": "Find the total duration of all tracks.",
+        "query": "SELECT SUM(Milliseconds) FROM Track;",
+    },
+    {
+        "input": "List all customers from Canada.",
+        "query": "SELECT * FROM Customer WHERE Country = 'Canada';",
+    },
+    {
+        "input": "How many tracks are there in the album with ID 5?",
+        "query": "SELECT COUNT(*) FROM Track WHERE AlbumId = 5;",
+    },
+    {
+        "input": "Find the total number of invoices.",
+        "query": "SELECT COUNT(*) FROM Invoice;",
+    },
+    {
+        "input": "List all tracks that are longer than 5 minutes.",
+        "query": "SELECT * FROM Track WHERE Milliseconds > 300000;",
+    },
+    {
+        "input": "Who are the top 5 customers by total purchase?",
+        "query": "SELECT CustomerId, SUM(Total) AS TotalPurchase FROM Invoice GROUP BY CustomerId ORDER BY TotalPurchase DESC LIMIT 5;",
+    },
+    {
+        "input": "Which albums are from the year 2000?",
+        "query": "SELECT * FROM Album WHERE strftime('%Y', ReleaseDate) = '2000';",
+    },
+    {
+        "input": "How many employees are there",
+        "query": 'SELECT COUNT(*) FROM "Employee"',
+    },
+]
+
+example_prompt = PromptTemplate.from_template("User input: {input}\nSQL query: {query}")
+few_shot_prompt = FewShotPromptTemplate(
+    examples = examples,
+    example_prompt = example_prompt,
+    prefix="You are a SQLite expert. Given an input question, create a syntactically correct SQLite query to run. Unless otherwise specificed, do not return more than {top_k} rows.\n\nHere is the relevant table info: {table_info}\n\nBelow are a number of examples of questions and their corresponding SQL queries.",
+    suffix="User input: {input}\nSQL query: ",
+    input_variables=["input", "top_k", "table_info"],
+)
+
+llm = ChatOllama(
+    model = "llama3.1",
+    temperature = 0,
+    num_predict = 256,
+)
+
+chain = few_shot_prompt | llm.bind(stop = ["\n"])
+response = chain.invoke({"input": "how many artists are there?", "top_k": 3, "table_info": "Artists"})
+print(response)
