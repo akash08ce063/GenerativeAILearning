8f8f17bcc30781524385987efbb9518bf1307dff
akash08ce063 <akashkpatel1991@gmail.com>
2024-12-16 03:24:28 -0800

    Create multi_agent_research_chart_generator_POC.py

---
+++ sample_codes/multi_agent_research_chart_generator_POC.py
@@ -0,0 +1,148 @@
+import json
+import os
+from langchain_core.messages import (
+    AIMessage,
+    BaseMessage,
+    ChatMessage,
+    FunctionMessage,
+    HumanMessage,
+)
+import operator
+from langchain_ollama import ChatOllama
+from langchain.tools.render import format_tool_to_openai_function
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langgraph.graph import END, StateGraph, MessagesState, START
+from langgraph.prebuilt.tool_executor import ToolExecutor, ToolInvocation
+from langchain_core.tools import tool
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langgraph.prebuilt import create_react_agent
+from langchain_experimental.utilities import PythonREPL
+from typing import Annotated, List, Sequence, Tuple, TypedDict, Union, Literal
+from langgraph.types import Command
+
+
+
+os.environ['TAVILY_API_KEY'] = "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"
+
+tavily_tool = TavilySearchResults(max_results=5)
+
+repl = PythonREPL()
+
+
+@tool
+def python_repl_tool(
+    code: Annotated[str, "The python code to execute to generate your chart."],
+):
+    """Use this to execute python code. If you want to see the output of a value,
+    you should print it out with `print(...)`. This is visible to the user."""
+    try:
+        result = repl.run(code)
+    except BaseException as e:
+        return f"Failed to execute. Error: {repr(e)}"
+    result_str = f"Successfully executed:\n```python\n{code}\n```\nStdout: {result}"
+    return (
+        result_str + "\n\nIf you have completed all tasks, respond with FINAL ANSWER."
+    )
+
+
+def make_system_prompt(suffix: str) -> str:
+    return (
+        "You are a helpful AI assistant, collaborating with other assistants."
+        " Use the provided tools to progress towards answering the question."
+        " If you are unable to fully answer, that's OK, another assistant with different tools "
+        " will help where you left off. Execute what you can to make progress."
+        " If you or any of the other assistants have the final answer or deliverable,"
+        " prefix your response with FINAL ANSWER so the team knows to stop."
+        f"\n{suffix}"
+    )
+
+
+model = ChatOllama(
+    model = "llama3.1",
+    temperature = 0,
+    num_predict = 256,
+) #.bind_tools(tools)
+
+def get_next_node(last_message : BaseMessage, goto : str):
+    if "FINAL ANSWER" in last_message.content:
+        return END
+    return goto
+
+research_agent = create_react_agent(
+    model=model,
+    tools=[tavily_tool],
+    state_modifier=make_system_prompt(
+        "You can only do research. You are working with a chart generator colleague."
+    ),
+)
+
+def research_node(
+    state: MessagesState,
+) -> Command[Literal["chart_generator", END]]:
+    result = research_agent.invoke(state)
+    goto = get_next_node(result["messages"][-1], "chart_generator")
+    # wrap in a human message, as not all providers allow
+    # AI message at the last position of the input messages list
+    result["messages"][-1] = HumanMessage(
+        content=result["messages"][-1].content, name="researcher"
+    )
+    return Command(
+        update={
+            # share internal message history of research agent with other agents
+            "messages": result["messages"],
+        },
+        goto=goto,
+    )
+
+
+# Chart generator agent and node
+# NOTE: THIS PERFORMS ARBITRARY CODE EXECUTION, WHICH CAN BE UNSAFE WHEN NOT SANDBOXED
+chart_agent = create_react_agent(
+    model=model,
+    tools = [python_repl_tool],
+    state_modifier=make_system_prompt(
+        "You can only generate charts. You are working with a researcher colleague."
+    ),
+)
+
+def chart_node(state: MessagesState) -> Command[Literal["researcher", END]]:
+    result = chart_agent.invoke(state)
+    goto = get_next_node(result["messages"][-1], "researcher")
+    # wrap in a human message, as not all providers allow
+    # AI message at the last position of the input messages list
+    result["messages"][-1] = HumanMessage(
+        content=result["messages"][-1].content, name="chart_generator"
+    )
+    return Command(
+        update={
+            # share internal message history of chart agent with other agents
+            "messages": result["messages"],
+        },
+        goto=goto,
+    )
+
+
+workflow = StateGraph(MessagesState)
+workflow.add_node("researcher", research_node)
+workflow.add_node("chart_generator", chart_node)
+
+workflow.add_edge(START, "researcher")
+graph = workflow.compile()
+
+events = graph.stream(
+    {
+        "messages": [
+            (
+                "user",
+                "First, get the UK's GDP over the past 5 years, then make a line chart of it. "
+                "Once you make the chart, finish.",
+            )
+        ],
+    },
+    # Maximum number of steps to take in the graph
+    {"recursion_limit": 150},
+)
+
+for s in events:
+    print(s)
+    print("----")
